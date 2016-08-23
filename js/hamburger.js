c55a75efa49875c182b54592d2351edc7c09bac5
Jared <jaredcmurphy@gmail.com>
2016-08-23 19:41:12 -0400

    looks good on mobile -chrome

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,8 +1,6 @@
-console.log('ham.js loaded.')
 
 $(function(){
 	$('.hamburger-container, .cross-container, .menuOption').on('click',function(){
-		console.log('clicked.')
 	  $('.hamburger-container').toggleClass('open');
 		$('.cross-container').toggleClass('open');
 	})
